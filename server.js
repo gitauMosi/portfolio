e988df0aaaa75956009f2d85c90ea0ba349a061f
gitauMosi <mosigitau@gmail.com>
2024-11-12 08:22:07 +0300

    added nodejs server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+//handle errors
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
+
+
+//run server
+app.listen(PORT, (err, req, res, next) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`Server is running on port ${PORT}`);
+    }
+
+});
