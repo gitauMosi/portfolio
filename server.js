0e95a57726a9959a99488156e2f898d2c900ee11
gitauMosi <mosigitau@gmail.com>
2024-11-29 12:16:24 +0300

    added projects and changed images

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 4000;
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
@@ -19,19 +21,17 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-
-
 //handle errors
 app.use((err, req, res, next) => {
-   
-    res.status(500).json({ error: err.message });
-});
+
+    res.status(500).send('Something went wrong!');
+}); 
 
 
 //run server
 app.listen(PORT, (err, req, res, next) => {
     if (err) {
-        console.error(err);
+        //console.error(err);
     } else {
         console.log(`Server is running on port ${PORT}`);
     }
